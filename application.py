1475700d0788d143c8529100a9351a634c248d2c
dmr <prasadrajankalwar@gmail.com>
2021-06-12 12:40:00 +0545

    initial commit

---
+++ application.py
@@ -0,0 +1,22 @@
+import discord
+import os
+client = discord.Client()
+from dotenv import load_dotenv
+load_dotenv()
+
+token = os.environ.get('discord_bot_token')
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello, I AM A PY-BOT!')
+
+client.run(token)
